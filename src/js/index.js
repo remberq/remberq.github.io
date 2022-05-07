4abd45e92c6b46286637469540b5e6c05ac39a99
remberq <rember.out@gmail.com>
2022-05-08 01:39:26 +0300

    add mobile size without JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
 import '../scss/style.scss';
+import '../scss/normalize.scss';
+import '../scss/fonts.scss';
+import '../scss/header.scss';
+import '../scss/variables.scss';
+import '../scss/hero.scss';
+import '../scss/brand.scss';
+import '../scss/repair.scss';
+import '../scss/price.scss';
+import '../scss/footer.scss';
+
+
+import '../js/swiper';
+
+
 
 console.log('Works!');
---
+++ src/js/swiper.js
@@ -0,0 +1,43 @@
+
+new Swiper('.hero--swiper', {
+  loop: true,
+  slidesPerView: 1.2,
+  spaceBetween: 16,
+  speed: 600,
+})
+
+
+new Swiper('.brand--swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  slidesPerView: 1.3,
+  spaceBetween: 16,
+  speed: 600,
+})
+
+
+new Swiper('.swiper--repair', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  slidesPerView: 1.3,
+  spaceBetween: 16,
+  speed: 600,
+})
+
+
+new Swiper('.price--swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  slidesPerView: 1.3,
+  spaceBetween: 16,
+  speed: 600,
+})
